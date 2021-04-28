bf27d6dbc786d1305309653e4fa5cfe8ac7c6579
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-04-28 13:16:17 +0300

    setup initial TS files

---
+++ showThatDiff/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./App.css";
+import { Header } from "./components/Header";
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
--- showThatDiff/src/content.ts
+++ showThatDiff/src/content.ts
@@ -1,2 +1,2 @@
 // This file is injected as a content script
-console.log("Hello from content script!")
+console.log("Hello from content");
