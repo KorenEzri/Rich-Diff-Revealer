3ca157d20dd95b2327523b5bf26692bbf1c1a9d4
Queefburglar <73742515+Queefburglar@users.noreply.github.com>
2021-04-28 20:24:54 +0300

    Update popup.tsx

--- showThatDiff/src/popup.tsx
+++ showThatDiff/src/popup.tsx
@@ -6,3 +6,16 @@ import "./popup.css";
 
 var mountNode = document.getElementById("popup");
 ReactDOM.render(<App />, mountNode);
+
+function injectTheScript() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // query the active tab, which will be only one tab
+        //and inject the script in it
+        chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
+    });
+}
+const allRelevantButtons = document.getElementsByClassName("octicon octicon-file")
+console.log("CHECL: ", allRelevantButtons)
+// allRelevantButtons.forEach(button => {
+//     button.addEventListener('click', injectTheScript);
+// })
