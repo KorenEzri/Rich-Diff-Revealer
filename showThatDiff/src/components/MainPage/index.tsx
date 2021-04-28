09027f6fff6aee24cd927e618306099a742b6256
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-04-29 01:13:25 +0300

    .

--- showThatDiff/src/components/MainPage/index.tsx
+++ showThatDiff/src/components/MainPage/index.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import {Header} from "../Header"
-import {ActivateButton} from "../ActivateButton"
 export const MainPage = () => {
   return (<div className="homepage_container">
     <div></div><Header />
-    <div><ActivateButton/></div>
   </div>);
 };
--- showThatDiff/src/content.ts
+++ showThatDiff/src/content.ts
@@ -1,6 +1,5 @@
 // This file is injected as a content script
-export const enableAutomaticRichDiffs = () => {
-    console.log("IN AUTO RICHDIFS")
+ const enableAutomaticRichDiffs = () => {
     const allRelevantButtons = document.getElementsByClassName("btn btn-sm BtnGroup-item tooltipped tooltipped-w rendered js-rendered")
     Array.from(allRelevantButtons).forEach(button => {
     if (button instanceof HTMLElement) {
@@ -8,3 +7,6 @@ export const enableAutomaticRichDiffs = () => {
     }
     })
 }
+window.addEventListener("blur", enableAutomaticRichDiffs)
+
+enableAutomaticRichDiffs()
