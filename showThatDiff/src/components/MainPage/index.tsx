d3e1089d19284a3ece8c91b0234b2339323673cd
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-05-02 18:29:13 +0300

    add images for links

--- showThatDiff/src/components/MainPage/index.tsx
+++ showThatDiff/src/components/MainPage/index.tsx
@@ -1,8 +1,29 @@
 import * as React from "react";
 import {Header} from "../Header"
+import { ReactTinyLink } from "react-tiny-link";
+import * as gitmage from "./gitmage.jpg"
+import * as revealer from "./revealer.jpg"
 
 export const MainPage = () => {
   return (<div className="homepage_container">
     <div className="main_header"><Header /></div>
+    <div className="github_link">
+    <ReactTinyLink
+        cardSize="small"
+        showGraphic={false}
+        defaultMedia={gitmage}
+        maxLine={2}
+        minLine={1}
+        url="https://github.com/KorenEzri"
+      />
+          <ReactTinyLink
+        defaultMedia={revealer}
+        cardSize="small"
+        showGraphic={false}
+        maxLine={2}
+        minLine={1}
+        url="https://github.com/KorenEzri/Rich-Diff-Revealer"
+      />
+    </div>
   </div>);
 };
--- showThatDiff/src/index.d.ts
+++ showThatDiff/src/index.d.ts
@@ -1,5 +1,7 @@
 declare module "pixelmatch";
+declare module "react-tiny-link";
 declare module "*.svg" {
   const content: any;
   export default content;
 }
+declare module "*.jpg";
